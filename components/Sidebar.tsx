af824debe4b766e4f44fa07638ec0b92ff176ad1
رموز احمد <zoor04303@gmail.com>
2026-02-27 06:29:20 +0300

    Create Sidebar.tsx

---
+++ components/Sidebar.tsx
@@ -0,0 +1,9 @@
+export default function Sidebar() {
+  return (
+    <aside style={{ width: "200px", background: "#1e293b", padding: "20px" }}>
+      <p>الرئيسية</p>
+      <p>الحملات</p>
+      <p>الإعدادات</p>
+    </aside>
+  )
+}
